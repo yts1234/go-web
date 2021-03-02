b2f0e63036df47b7521f5fdee2a283f34574590f
yosua <yosua@jojonomic.com>
2021-03-02 10:55:19 +0000

    Refactor test file.

--- handler/iteration/iteration.go
+++ handler/iteration/iteration.go
@@ -2,6 +2,7 @@ package iteration
 
 const repeatCount = 5
 
+// Repeat will return repeated n count word
 func Repeat(char string, count int) string {
 	var repeated string
 	if count == 0 {
@@ -10,8 +11,8 @@ func Repeat(char string, count int) string {
 		}
 		return repeated
 	}
-	repeatCount := count
-	for i := 0; i < repeatCount; i++ {
+
+	for i := 0; i < count; i++ {
 		repeated += char
 	}
 	return repeated
--- handler/iteration/iteration_test.go
+++ handler/iteration/iteration_test.go
@@ -3,22 +3,23 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	assertCorrectMessage := func(t )
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("expected %q but got %q", got, want)
+		}
+	}
 	t.Run("Repeat word printing by repeated n count supplied, default 5", func(t *testing.T) {
 		repeated := Repeat("a", 0)
 		expected := "aaaaa"
 
-		if repeated != expected {
-			t.Errorf("expected %q but got %q", expected, repeated)
-		}
+		assertCorrectMessage(t, repeated, expected)
 	})
 	t.Run("Repeat word printing by repeated count supplied, repeat count = 10", func(t *testing.T) {
 		repeated := Repeat("a", 10)
 		expected := "aaaaaaaaaa"
 
-		if repeated != expected {
-			t.Errorf("expected %q but got %q", expected, repeated)
-		}
+		assertCorrectMessage(t, repeated, expected)
 	})
 }
 
