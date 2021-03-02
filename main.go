429886e84c631124a54ab2946bfa44bdf94dad6d
yosua <yosua@jojonomic.com>
2021-03-02 08:03:49 +0000

    Add new test-branch, content: Excercise with TDD

--- main.go
+++ main.go
@@ -1,32 +1,35 @@
 package main
 
-import (
-	"log"
-	"net/http"
+import "fmt"
 
-	"github.com/yts1234/go-web/handler/httphandler"
-	"github.com/yts1234/go-web/handler/jwthandler"
-	"github.com/yts1234/go-web/handler/pasetohandler"
-)
+// Main func
+// func main() {
+// 	mux := http.NewServeMux()
+
+// 	mux.HandleFunc("/", httphandler.HomeHandler)
+// 	mux.HandleFunc("/helo", httphandler.HelloHandler)
+// 	mux.HandleFunc("/product", httphandler.ProductHandler)
+// 	mux.HandleFunc("/welcome", httphandler.WelcomeHandler)
+// 	mux.HandleFunc("/signin", jwthandler.Signin)
+// 	mux.HandleFunc("/pasetosignin", pasetohandler.PasetoSignIn)
+// 	mux.HandleFunc("/welcomepaseto", httphandler.WelcomePaseto)
+
+// 	fileServer := http.FileServer(http.Dir("assets"))
+// 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+// 	log.Println("Starting web on port 8080")
+
+// 	err := http.ListenAndServe(":8080", mux)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
+// Test function for exercise
+func Hello(name string) string {
+	return "Hello, " + name
+}
 
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", httphandler.HomeHandler)
-	mux.HandleFunc("/helo", httphandler.HelloHandler)
-	mux.HandleFunc("/product", httphandler.ProductHandler)
-	mux.HandleFunc("/welcome", httphandler.WelcomeHandler)
-	mux.HandleFunc("/signin", jwthandler.Signin)
-	mux.HandleFunc("/pasetosignin", pasetohandler.PasetoSignIn)
-	mux.HandleFunc("/welcomepaseto", httphandler.WelcomePaseto)
-
-	fileServer := http.FileServer(http.Dir("assets"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	log.Println("Starting web on port 8080")
-
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(Hello("world"))
 }
