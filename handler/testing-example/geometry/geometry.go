077f2e7ffb3baf0bb3cb251ae2a123a9db412ae0
yosua <yosua@jojonomic.com>
2021-03-03 04:04:06 +0000

    Add struct and interface for object implementation Add checkArea test helper

---
+++ handler/testing-example/geometry/geometry.go
@@ -0,0 +1,28 @@
+package geometry
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Width + r.Height)
+}
