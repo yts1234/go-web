54fc97dcfdb33d4b5b7be47189f22394b52749a7
yosua <yosua@jojonomic.com>
2021-03-03 07:24:33 +0000

    Separate assert function Add assertNoError for check if it does not have any error

---
+++ handler/testing-example/bitcoin/bitcoin_test.go
@@ -0,0 +1,55 @@
+package bitcoin
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
+	})
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		Wallet := Wallet{balance: startingBalance}
+		err := Wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, Wallet, startingBalance)
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatalf("got an error but didn't want one")
+	}
+}
